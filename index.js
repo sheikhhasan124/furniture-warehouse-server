eafa7428a52574341eafc056938bf7952db81e9d
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-30 05:55:07 +0600

    data loaded from database to server

--- index.js
+++ index.js
@@ -14,12 +14,25 @@ app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://furnitureWarehouse:0YlFGborr8mxojzG@cluster0.hjxrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('connect db')
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+try {
+  await client.connect()
+ const furnitureCollections = client.db('furniture').collection('products')
+
+// get products api
+app.get('/products',async(req,res)=>{
+  const query = {}
+  const cursor = furnitureCollections.find(query);
+  const products = await cursor.toArray()
+  // res.send('ok')
+  res.send(products)
+})
+
+} finally {
+  // await client.close();
+}
+}
+run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
