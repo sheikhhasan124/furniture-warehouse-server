f411c8dc398a98bf0ea4f4f7effab61f5208cf18
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-30 05:33:39 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://furnitureWarehouse:0YlFGborr8mxojzG@cluster0.hjxrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('connect db')
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.get('/',(req,res)=>{
     res.send('home')
 })
