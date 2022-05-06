4e9439adbd5ba2f0b21a7b06c98d845529e69cb3
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-06 19:46:46 +0600

    update quaintity success

--- index.js
+++ index.js
@@ -18,7 +18,18 @@ async function run(){
 try {
   await client.connect()
  const furnitureCollections = client.db('furniture').collection('products')
+ const furnitureGallay = client.db('furniture').collection('gallary')
+ const furnituQuantity = client.db('furniture').collection('quantity')
 
+
+
+ // get all img from gallary
+ app.get('/gallary',async(req,res)=>{
+   const query = {}
+   const cursor = furnitureGallay.find(query);
+   const imges = await cursor.toArray()
+   res.send(imges)
+ })
 // post api
 app.post('/product',async(req,res)=>{
    const newProduct = req.body;
@@ -32,6 +43,20 @@ app.get('/product',async(req,res)=>{
   const products = await cursor.toArray()
   // res.send('ok')
   res.send(products)
+})
+// UPDATE PRODUCT
+app.put('/product/:id',async(req,res)=>{
+  const id = req.params.id;
+  const updateQuantity = req.body;
+  const filter = {_id:ObjectId(id)}
+  const option = {upsert:true}
+  const updateDoc = {
+    $set:{
+      quantity:updateQuantity.quantity
+    }
+  }
+  const result = await furnitureCollections.updateOne(filter, updateDoc, option);
+  res.send(result)
 })
 // get a product api
 app.get('/product/:id',async(req,res)=>{
