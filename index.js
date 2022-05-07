ca8c6a3ddef583aebf0e144fb897ff4b531506e3
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-07 14:56:08 +0600

    git ignore dose not work

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 
-require('dotenv').config();
+require('dotenv').config()
 var jwt =require('jsonwebtoken')
 
 const port = process.env.PORT || 5000;
